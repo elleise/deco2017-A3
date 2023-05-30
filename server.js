14873f984ba3d2c6a367f89dbd87d2a36b977d87
Elleise Boon <elleiseboon9@gmail.com>
2023-05-30 23:11:03 +1000

    this is the initial version of my implementation

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html')
    })
 
-   let server = app.listen(8888, function(){
-    console.log("App server is running on port 8888");
+   let server = app.listen(8889, function(){
+    console.log("App server is running on port 8889");
    });
