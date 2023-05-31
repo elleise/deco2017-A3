2584bedd34190757a549d8e71c6217cc5a573d3c
Elleise Boon <elleiseboon9@gmail.com>
2023-05-31 21:48:52 +1000

    Modal box is displayed with x button when the user clicks the image displayed in song item list

--- public/songs.js
+++ public/songs.js
@@ -24,7 +24,7 @@ function displaySong(song) {
       </div>
       <div class="item-song-row">
           <div class="item-image">
-              <img src="image1.jpg" alt="Girl in a jacket" width="40" height="40">
+              <img id="myImg" src="image1.jpg" alt='${song.description}' width="40" height="40" onclick="image(this)">
           </div>
           <div class="item-song-name">
               <b>${song.songName}</b> <br> ${song.artist}
@@ -35,14 +35,8 @@ function displaySong(song) {
       </div>
     </div> `;
 
-/*
-    `<div>${song.mood}</div><div><strong>${song.songName}</strong><br>${song.artist}</p>
-     <p>${song.genre}<br>${song.mood}</p></div>
-    `;
-*/
   songlistElem.appendChild(item);
   form.reset();
-
 }
 
 // Create an array called 'taskList'
@@ -74,3 +68,33 @@ addSong("Working", "Sam Hui", "Genre", "Relaxed", "Relaxed song from Sam");
 
 // Log the array to the console.
 console.log(songList);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+/*
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+*/
+
+function image(img) {
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
