616e6416c65a040087e2cd57ca2d51bf136ad72c
Elleise Boon <elleiseboon9@gmail.com>
2023-06-01 16:40:34 +1000

    JS code can successfully delete li items

--- public/songs.js
+++ public/songs.js
@@ -1,5 +1,10 @@
+// Setting up variables for HTML elements using DOM selection, including form element and song list elements under form
 const form = document.getElementById('songform');
 const songlistElem = document.querySelector('#songs');
+
+//  event after log entry button 
+//  add song will add all values including id
+//  push into songlist and call displaySong
 
 form.addEventListener('submit', function(event){
   // Block the default submission behaviour
@@ -14,9 +19,13 @@ form.addEventListener('submit', function(event){
   );
 })
 
+// data id is the date, create class for li - allows me to search up all the li items for last step to remove record
+// 7 blocks from list item inide one div to remove an item. you need to find the li through its id set by setattribute, where we set the song.id to the dataid
+
 function displaySong(song) {
   let item = document.createElement('li');
-  item.setAttribute('data-id', song.id);
+  item.setAttribute('dataid', song.id);
+  item.className = "songs-li";
   item.innerHTML = 
     `<div class="item">
       <div class="item-mood-row">
@@ -24,7 +33,7 @@ function displaySong(song) {
       </div>
       <div class="item-song-row">
           <div class="item-image">
-              <img id="myImg" src="image1.jpg" alt='${song.description}' width="40" height="40" onclick="image(this)">
+              <img id="myImg" src="image1.jpg" alt='${song.description}' width="40" height="40" onclick="image(this, ${song.id})">
           </div>
           <div class="item-song-name">
               <b>${song.songName}</b> <br> ${song.artist}
@@ -39,19 +48,15 @@ function displaySong(song) {
   form.reset();
 }
 
+// song.id displays image
 // Create an array called 'taskList'
 var songList = [];
-
-// Create a function called 'addTask'
-// Give the function input parameters for: name, type, rate, time, client
-// Paste your object definition from above in the function
-// Replace the property values with the input paramaters
-// Add the object to the taskList array
 
 function addSong(songName, artist, genre, mood, description) {
 
   // Creating the object, directly passing in the input parameters
   let song = {
+    id: Date.now(),
     songName,
     artist,
     genre,
@@ -76,19 +81,29 @@ var modal = document.getElementById("myModal");
 //var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+var delButton = document.getElementsByClassName("del-button")[0];
+var modalItem = null;
+var modalLiItem = null;
 
-/*
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-*/
+// block summons screen, src loads the image , img.alt displays description, find out which li item has the song item
+// when you clickl delete, this findLiElementById tells me which modal box correponds to a specific song item 
 
-function image(img) {
+function image(img, songId) {
   modal.style.display = "block";
   modalImg.src = img.src;
   captionText.innerHTML = img.alt;
+  modalItem = img;
+  modalLiItem = findLiElementById(songId);
+}
+
+function findLiElementById(songId) {
+  var liItems = document.getElementsByClassName("songs-li");
+  for (i = 0; i < liItems.length; i++) {
+    if (liItems[i].getAttribute("dataid") == songId) {
+      console.log(songId);
+      return liItems[i];
+    }
+  }  
 }
 
 // Get the <span> element that closes the modal
@@ -97,4 +112,22 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
+}
+console.log(songList);
+
+
+delButton.addEventListener('click', handleDelEvent);
+
+function handleDelEvent(event) {
+  console.log(songList);
+  console.log(modalItem.alt);
+  
+  modalLiItem.remove();
+  songList.forEach(function(songArrayElement, songArrayIndex){
+    if (songArrayElement.id == modalItem.getAttribute('dataid')) {
+      songList.splice(songArrayIndex, 1);
+    }
+  })
+
+  console.log(songList);
 }
